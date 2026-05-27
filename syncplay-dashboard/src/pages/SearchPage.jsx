9201ae186218adc8e46219ce8d581378adae01ea
JuneYeop00 <wnsduq2003@gmail.com>
2026-05-27 16:09:02 +0900

    configure search page API base URL

--- syncplay-dashboard/src/pages/SearchPage.jsx
+++ syncplay-dashboard/src/pages/SearchPage.jsx
@@ -7,7 +7,6 @@ import API_BASE_URL from '../config/api';
 const TMDB_ACCESS_TOKEN = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 
-
 const LOGO_MAP = {
   "netflix": "/logos/netflix.png",
   "tving": "/logos/tving.png",
